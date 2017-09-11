0f12dd5c2e6e1e9c7b6ca7cd0edd460c99f48fdc
Neous1 <Neous1@users.noreply.github.com>
2017-09-11 11:46:37 -0400

    updated day3

---
+++ day3_Conditional_Statements.js
@@ -0,0 +1,42 @@
+
+/**
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5 , print Not Weird
+If n is even and in the inclusive range of 6 to 20 , print Weird
+If n is even and greater than 20 , print Not Weird
+*/
+
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var N = parseInt(readLine());
+
+    
+    if (N %2 === 1 || N > 5 && N < 21){
+        console.log("Weird");
+    }else {
+        console.log("Not Weird");
+    }
+
+}
